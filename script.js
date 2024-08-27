cd2621011c2d771b22b29f51d86aa85739f6a3a9
yahyasohaill <161240080+yahyasohaill@users.noreply.github.com>
2024-08-27 09:51:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const imagesDisplay = document.getElementById("images-display");
+const moreBtn = document.getElementById("more-btn");
+const accessKey = "ekDCjVB8nx80ARszzQpBqfKRrDu4OissJkBl51ddBJQ";
+searchForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    page=1;
+    searchImg();
+})
+let search = "";
+let page = 1;
+async function searchImg(){
+   search = searchBox.value;
+   const url = `https://api.unsplash.com/search/collections?page=${page}&query=${search}&client_id=${accessKey}&per_page=15`;
+
+   const response = await fetch(url);
+   const data = await response.json();
+   const results = data.results;
+   if(page==1){
+       imagesDisplay.innerHTML = "";
+   }
+   results.map(results =>{
+       const image = document.createElement("img");
+       image.src = results.cover_photo.urls.small;
+       image.setAttribute('data-aos','fade-up');
+       image.setAttribute('loading','lazy');
+
+       const imageLink = document.createElement("a");
+       imageLink.href = results.links.html;
+       imageLink.target = "_blank";
+       
+       //    imageLink.setAttribute=('data-aos','fade-up');
+       
+       imageLink.appendChild(image);
+       
+       
+    //    imagesDisplay.appendChild(imageLink);
+        imagesDisplay.appendChild(imageLink);
+    })
+    moreBtn.style.display = "block"
+}
+
+moreBtn.addEventListener("click",e=>{
+    e.preventDefault();
+    page++;
+    searchImg();
+})
